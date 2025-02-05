f58f30a947e31fa27ecb128cfff6cac5ad0e65b4
steven200292 <stevenchaput562@gmail.com>
2025-02-05 11:58:50 +0100

    Update senoo.ino

--- Steven/senoo/senoo.ino
+++ Steven/senoo/senoo.ino
@@ -1,1 +1,25 @@
-test
+#include "DFRobot_VEML7700.h"
+#include <Wire.h>
+
+/*
+ * Instantiate an object to drive the sensor
+ */
+DFRobot_VEML7700 als;
+
+void setup()
+{
+  Serial.begin(9600);
+  als.begin();   // Init
+}
+
+void loop()
+{
+  float lux;
+  
+  als.getALSLux(lux);   // Get the measured ambient light value
+  Serial.print("Lux:");
+  Serial.print(lux);
+  Serial.println(" lx");
+  
+  delay(200);
+}
